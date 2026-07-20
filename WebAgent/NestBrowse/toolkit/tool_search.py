425bd554ead51ac1f5751e752cc5892b28e30cd2
lalelu <whonix@mytsunami.de>
2026-07-20 19:39:43 +0200

    chore: snapshot commit before bulk merge

--- WebAgent/NestBrowse/toolkit/tool_search.py
+++
@@ -1,5 +0,0 @@
-
-
-'''
-The configuration for the search tool can be obtained from the **Inference** folder in the **DeepResearch** repository.
-'''
--- WebAgent/WebWatcher/infer/vl_search_r1/qwen-agent-o1_search/qwen_agent/tools/gpt4o/constant.py
+++
@@ -1,8 +0,0 @@
-SUPPORT_ARGS = {"model", "messages", "frequency_penalty", "logit_bias",
-                "logprobs", "top_logprobs",
-                "max_completion_tokens", "max_tokens",
-                "n", "presence_penalty", "response_format", "seed", "stop", "stream",
-                "temperature", "top_p",
-                "tools", "tool_choice", "user", "function_call", "functions", "tenant",
-                "modalities", "audio",
-                "baseUrl", "min_p", "min_a"}
